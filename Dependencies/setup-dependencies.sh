548c1247e78e0eee9a24fb133b7213b91e598f85
iOmega8561 <giusepperocco38@gmail.com>
2024-12-10 12:21:18 +0100

    Moved setup-dependencies.sh to Dependencies/

--- Dependencies/setup-dependencies.sh
+++ Dependencies/setup-dependencies.sh
@@ -3,58 +3,60 @@ SCRIPT=$(readlink -f $0)
 # Absolute path this script is in.
 SCRIPTPATH=`dirname $SCRIPT`
 
+cd $SCRIPTPATH
+
 # LIBPNG
-git clone https://github.com/glennrp/libpng.git
-mkdir libpng-build && cd libpng
+git clone https://github.com/glennrp/libpng.git libpng-src
+mkdir libpng && cd libpng-src
 
 mkdir build && cd build && cmake .. \
   -DCMAKE_SYSTEM_NAME=Darwin \
   -DCMAKE_OSX_ARCHITECTURES=arm64 \
   -DCMAKE_OSX_SYSROOT=$(xcrun --sdk xros --show-sdk-path) \
-  -DCMAKE_INSTALL_PREFIX=$SCRIPTPATH/libpng-build
+  -DCMAKE_INSTALL_PREFIX=$SCRIPTPATH/libpng
   
 make && make install && make clean && cd ../..
 
 # OPENSSL
-git clone https://github.com/openssl/openssl.git
-mkdir openssl-build && cd openssl
+git clone https://github.com/openssl/openssl.git openssl-src
+mkdir openssl && cd openssl-src
 
 ./Configure ios64-cross \
   no-shared \
-  --prefix=$SCRIPTPATH/openssl-build \
+  --prefix=$SCRIPTPATH/openssl \
   -isysroot $(xcrun --sdk xros --show-sdk-path)
   
 make && make install && make clean && cd ..
 
 # ZLIB
-git clone https://github.com/madler/zlib.git
-mkdir zlib-build && cd zlib
+git clone https://github.com/madler/zlib.git zlib-src
+mkdir zlib && cd zlib-src
 
 SDKROOT=$(xcrun --sdk xros --show-sdk-path) \
 CC="clang -isysroot $SDKROOT -arch arm64" \
 ./configure \
   --static \
-  --prefix=$SCRIPTPATH/zlib-build
+  --prefix=$SCRIPTPATH/zlib
 
 make && make install && make clean && cd ..
 
 # DCMTK
-# git clone https://github.com/DCMTK/dcmtk.git
-mkdir dcmtk-build && cd dcmtk
+# git clone https://github.com/DCMTK/dcmtk.git dcmtk-src
+mkdir dcmtk && cd dcmtk-src
 
 mkdir build && cd build && cmake .. \
   -DBUILD_SHARED_LIBS=OFF \
   -DCMAKE_SYSTEM_NAME=Darwin \
   -DCMAKE_OSX_ARCHITECTURES=arm64 \
   -DCMAKE_OSX_SYSROOT=$(xcrun --sdk xros --show-sdk-path) \
-  -DCMAKE_INSTALL_PREFIX=$SCRIPTPATH/dcmtk-build \
-  -DPNG_LIBRARY=$SCRIPTPATH/libpng-build/lib/libpng.a \
-  -DPNG_PNG_INCLUDE_DIR=$SCRIPTPATH/libpng-build/include \
-  -DOPENSSL_ROOT_DIR=$SCRIPTPATH/libssl-build \
-  -DOPENSSL_LIBRARIES="$SCRIPTPATH/libssl-build/lib/libssl.a;$SCRIPTPATH/libssl-build/lib/libcrypto.a" \
-  -DOPENSSL_INCLUDE_DIR=$SCRIPTPATH/libssl-build/include \
-  -DZLIB_LIBRARY=$SCRIPTPATH/zlib-build/lib/libz.a \
-  -DZLIB_INCLUDE_DIR=$SCRIPTPATH/zlib-build/include \
+  -DCMAKE_INSTALL_PREFIX=$SCRIPTPATH/dcmtk \
+  -DPNG_LIBRARY=$SCRIPTPATH/libpng/lib/libpng.a \
+  -DPNG_PNG_INCLUDE_DIR=$SCRIPTPATH/libpng/include \
+  -DOPENSSL_ROOT_DIR=$SCRIPTPATH/libssl \
+  -DOPENSSL_LIBRARIES="$SCRIPTPATH/libssl/lib/libssl.a;$SCRIPTPATH/libssl/lib/libcrypto.a" \
+  -DOPENSSL_INCLUDE_DIR=$SCRIPTPATH/libssl/include \
+  -DZLIB_LIBRARY=$SCRIPTPATH/zlib/lib/libz.a \
+  -DZLIB_INCLUDE_DIR=$SCRIPTPATH/zlib/include \
   -DCMAKE_EXE_LINKER_FLAGS="-lz"
 
 make && make install && make clean && cd ../..
