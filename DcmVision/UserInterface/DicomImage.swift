89c0834a981d7f830fad54d6f3cd30b2b3fb20f1
iOmega8561 <giusepperocco38@gmail.com>
2024-12-11 15:12:13 +0100

    Propagated changes in User Interface files, mainly naming

---
+++ DcmVision/UserInterface/DicomImage.swift
@@ -0,0 +1,44 @@
+//
+//  DicomImage.swift
+//  DcmVision
+//
+//  Created by Giuseppe Rocco on 11/12/24.
+//
+
+import SwiftUI
+
+struct DicomImage: View {
+    
+    private static let dcmService: DicomService = .init()
+    
+    let fileName: String
+    
+    @State private var dicomImage: Image? = nil
+    
+    var body: some View {
+        
+        Group {
+            if let dicomImage = dicomImage {
+                
+                dicomImage
+                    .resizable()
+                    .scaledToFit()
+                
+            } else {
+                
+                Image(systemName: "exclamationmark.triangle.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: 100, maxHeight: 100)
+                    .foregroundStyle(.yellow)
+            }
+        }
+        .onAppear {
+            self.dicomImage = try? Image(
+                uiImage: Self.dcmService.uiImageFromFile(
+                    withName: fileName
+                )
+            )
+        }
+    }
+}
