9715aac2bdd8fe0e6e9721fb2f57bff4ee9ab91d
iOmega8561 <giusepperocco38@gmail.com>
2025-02-17 15:42:21 +0100

    Dependencies: - Dependencies is now a group instead of a folder in the xcode project - DataSet folder deleted - Created group DicomFiles

--- Dependencies/setup-dependencies.sh
+++ Dependencies/setup-dependencies.sh
@@ -6,57 +6,91 @@ SCRIPTPATH=`dirname $SCRIPT`
 cd $SCRIPTPATH
 
 # LIBPNG
-git clone https://github.com/glennrp/libpng.git libpng-src
-mkdir libpng && cd libpng-src
+if [ ! -d "libpng" ]; then
 
-mkdir build && cd build && cmake .. \
-  -DCMAKE_SYSTEM_NAME=Darwin \
-  -DCMAKE_OSX_ARCHITECTURES=arm64 \
-  -DCMAKE_OSX_SYSROOT=$(xcrun --sdk xros --show-sdk-path) \
-  -DCMAKE_INSTALL_PREFIX=$SCRIPTPATH/libpng
-  
-make && make install && make clean && cd ../..
+    if [ ! -d "libpng-src" ]; then
+        git clone https://github.com/glennrp/libpng.git libpng-src
+    fi
+
+    mkdir libpng && cd libpng-src
+    
+    mkdir build && cd build && cmake .. \
+        -DCMAKE_SYSTEM_NAME=Darwin \
+        -DCMAKE_OSX_ARCHITECTURES=arm64 \
+        -DCMAKE_OSX_SYSROOT=$(xcrun --sdk xros --show-sdk-path) \
+        -DCMAKE_INSTALL_PREFIX=$SCRIPTPATH/libpng
+    
+    make && make install && make clean && cd ../..
+fi
+
+rm -fr libpng-src
 
 # OPENSSL
-git clone https://github.com/openssl/openssl.git openssl-src
-mkdir openssl && cd openssl-src
+if [ ! -d "openssl" ]; then
 
-./Configure ios64-cross \
-  no-shared \
-  --prefix=$SCRIPTPATH/openssl \
-  -isysroot $(xcrun --sdk xros --show-sdk-path)
-  
-make && make install && make clean && cd ..
+    if [ ! -d "openssl-src" ]; then
+        git clone https://github.com/openssl/openssl.git openssl-src
+    fi
+
+    mkdir openssl && cd openssl-src
+
+    ./Configure ios64-cross \
+      no-shared \
+      --prefix=$SCRIPTPATH/openssl \
+      -isysroot $(xcrun --sdk xros --show-sdk-path)
+    
+    make && make install && make clean && cd ..
+fi
+
+rm -fr openssl-src
 
 # ZLIB
-git clone https://github.com/madler/zlib.git zlib-src
-mkdir zlib && cd zlib-src
+if [ ! -d "zlib" ]; then
 
-SDKROOT=$(xcrun --sdk xros --show-sdk-path) \
-CC="clang -isysroot $SDKROOT -arch arm64" \
-./configure \
-  --static \
-  --prefix=$SCRIPTPATH/zlib
+    if [ ! -d "zlib-src" ]; then
+        git clone https://github.com/madler/zlib.git zlib-src
+    fi
+    
+    mkdir zlib && cd zlib-src
+    
+    SDKROOT=$(xcrun --sdk xros --show-sdk-path) \
+    CC="clang -isysroot $SDKROOT -arch arm64" \
+    ./configure \
+      --static \
+      --prefix=$SCRIPTPATH/zlib
+    
+    make && make install && make clean && cd ..
+fi
 
-make && make install && make clean && cd ..
+rm -fr zlib-src
 
 # DCMTK
-# git clone https://github.com/DCMTK/dcmtk.git dcmtk-src
-mkdir dcmtk && cd dcmtk-src
+if [ ! -d "dcmtk" ]; then
 
-mkdir build && cd build && cmake .. \
-  -DBUILD_SHARED_LIBS=OFF \
-  -DCMAKE_SYSTEM_NAME=Darwin \
-  -DCMAKE_OSX_ARCHITECTURES=arm64 \
-  -DCMAKE_OSX_SYSROOT=$(xcrun --sdk xros --show-sdk-path) \
-  -DCMAKE_INSTALL_PREFIX=$SCRIPTPATH/dcmtk \
-  -DPNG_LIBRARY=$SCRIPTPATH/libpng/lib/libpng.a \
-  -DPNG_PNG_INCLUDE_DIR=$SCRIPTPATH/libpng/include \
-  -DOPENSSL_ROOT_DIR=$SCRIPTPATH/libssl \
-  -DOPENSSL_LIBRARIES="$SCRIPTPATH/libssl/lib/libssl.a;$SCRIPTPATH/libssl/lib/libcrypto.a" \
-  -DOPENSSL_INCLUDE_DIR=$SCRIPTPATH/libssl/include \
-  -DZLIB_LIBRARY=$SCRIPTPATH/zlib/lib/libz.a \
-  -DZLIB_INCLUDE_DIR=$SCRIPTPATH/zlib/include \
-  -DCMAKE_EXE_LINKER_FLAGS="-lz"
+    if [ ! -d "dcmtk-src" ]; then
+        git clone https://github.com/DCMTK/dcmtk.git dcmtk-src
+    fi
+    
+    mkdir dcmtk && cd dcmtk-src
+    
+    sed -i '' 's/ dcm2pdf//g' dcmdata/apps/CMakeLists.txt
+    
+    mkdir build && cd build && cmake .. \
+      -DBUILD_SHARED_LIBS=OFF \
+      -DCMAKE_SYSTEM_NAME=Darwin \
+      -DCMAKE_OSX_ARCHITECTURES=arm64 \
+      -DCMAKE_OSX_SYSROOT=$(xcrun --sdk xros --show-sdk-path) \
+      -DCMAKE_INSTALL_PREFIX=$SCRIPTPATH/dcmtk \
+      -DPNG_LIBRARY=$SCRIPTPATH/libpng/lib/libpng.a \
+      -DPNG_PNG_INCLUDE_DIR=$SCRIPTPATH/libpng/include \
+      -DOPENSSL_ROOT_DIR=$SCRIPTPATH/libssl \
+      -DOPENSSL_LIBRARIES="$SCRIPTPATH/libssl/lib/libssl.a;$SCRIPTPATH/libssl/lib/libcrypto.a" \
+      -DOPENSSL_INCLUDE_DIR=$SCRIPTPATH/libssl/include \
+      -DZLIB_LIBRARY=$SCRIPTPATH/zlib/lib/libz.a \
+      -DZLIB_INCLUDE_DIR=$SCRIPTPATH/zlib/include \
+      -DCMAKE_EXE_LINKER_FLAGS="-lz"
+    
+    make && make install && make clean && cd ../..
+fi
 
-make && make install && make clean && cd ../..
+rm -fr dcmtk-src
