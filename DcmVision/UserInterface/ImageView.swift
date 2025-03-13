8dfd321bef045bdefbbb462be04b3f1b548726b5
iOmega8561 <giusepperocco38@gmail.com>
2025-03-13 18:58:38 +0100

    Minor refinements: - File URLs now are sorted alphabetically before usage - ImageView, removed SwiftUI Group, moved .task to ProgressView - ModelView, improved overlay if statement to always check that modelEntity is nil

--- DcmVision/UserInterface/ImageView.swift
+++ DcmVision/UserInterface/ImageView.swift
@@ -10,44 +10,43 @@ import SwiftUI
 struct ImageView: View {
         
     let fileURL: URL
-    
     let showErrorDescription: Bool
     
     @State private var error: Error? = nil
-    
     @State private var dicomImage: Image? = nil
     @State private var metadata: [String: Any] = [:]
     
     var body: some View {
         
-        Group {
-            if let dicomImage = dicomImage {
-                
-                dicomImage
-                    .resizable()
-                    .scaledToFit()
-                    .overlay(alignment: .topLeading) {
+        if let dicomImage = dicomImage {
+            
+            dicomImage
+                .resizable()
+                .scaledToFit()
+                .overlay(alignment: .topLeading) {
+                    
+                    VStack(alignment: .leading) {
                         
-                        VStack(alignment: .leading) {
-                            
-                            ForEach(metadata.keys.sorted(), id: \.self) { key in
-                                Text("\(key): \(metadata[key]!)")
-                            }
+                        ForEach(metadata.keys.sorted(), id: \.self) { key in
+                            Text("\(key): \(metadata[key]!)")
                         }
-                        .foregroundStyle(.white)
-                        .padding()
                     }
-                
-            } else if let error {
-                showErrorDescription ? ErrorView(error: error) : ErrorView()
-                
-            } else { ProgressView().scaleEffect(1.5) }
-        }
-        .task {
-            do {
-                try bootstrap()
-                
-            } catch { self.error = error }
+                    .foregroundStyle(.white)
+                    .padding()
+                }
+            
+        } else if let error {
+            showErrorDescription ? ErrorView(error: error) : ErrorView()
+            
+        } else {
+            ProgressView()
+                .scaleEffect(1.5)
+                .task {
+                    do {
+                        try bootstrap()
+                        
+                    } catch { self.error = error }
+                }
         }
     }
     
