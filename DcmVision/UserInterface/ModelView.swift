8c617960c43c7f819af439a3c110e0c5c6edc71e
iOmega8561 <giusepperocco38@gmail.com>
2025-04-30 16:56:35 +0200

    Many changes - Moved model entity setup logic to a ModelEntity extension - Added a function to VTKWrapper to optimize the polyData

--- DcmVision/UserInterface/ModelView.swift
+++ DcmVision/UserInterface/ModelView.swift
@@ -21,43 +21,21 @@ struct ModelView: View {
         RealityView { _ in
             
             Task.detached(priority: .utility) {
+                
                 do {
+                
                     let isoSurface = try dataSet.isoSurface(huThreshold: 300)
-                    let modelEntity = try await ModelEntity(contentsOf: isoSurface)
+                    let modelEntity = try await ModelEntity.fromIsoSurface(at: isoSurface)
                     
                     await MainActor.run {
-                        
-                        modelEntity.transform.scale = [0.0015, 0.0015, 0.0015]
-                        
-                        modelEntity.transform.rotation = .init(
-                            angle: -.pi*1.5,
-                            axis: [1, 0, 0]
-                        )
-                        
-                        modelEntity.model?.materials = [
-                            SimpleMaterial(color: .white, isMetallic: false)
-                        ]
-                        
-                        let boundingBox = modelEntity.visualBounds(relativeTo: nil)
-                        let boundingBoxCenter = boundingBox.center
-                        
-                        modelEntity.position = [
-                            -boundingBoxCenter.x,
-                            -boundingBoxCenter.y + 1.5,
-                            -boundingBoxCenter.z - 1.5
-                        ]
-                        
-                        modelEntity.components.set(InputTargetComponent(allowedInputTypes: .indirect))
-                        modelEntity.generateCollisionShapes(recursive: true)
-                        modelEntity.components.set(GestureComponent())
-                        
-                        modelEntity.setDirectGestures(enabled: true)
-                        
                         self.modelEntity = modelEntity
                     }
-                } catch { await MainActor.run { self.error = error } }
+                    
+                } catch {
+                    
+                    await MainActor.run { self.error = error }
+                }
             }
-            
         } update: { content in
             
             if let modelEntity {
