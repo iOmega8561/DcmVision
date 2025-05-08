995a32c6018a13c7702f741bcb1dedb284f2fc30
iOmega8561 <giusepperocco38@gmail.com>
2025-05-08 11:39:15 +0200

    Small changes - Added a demo alert - Added AppIcon assets - Changed bundle identifier to .DcmVision - Changed version to 0.1

--- DcmVision/ApplicationCore/DcmVisionApp.swift
+++ DcmVision/ApplicationCore/DcmVisionApp.swift
@@ -11,6 +11,8 @@ import RealityKitContent
 @main
 struct DcmVisionApp: App {
             
+    @State private var alertForDemo = !UserDefaults.standard.bool(forKey: "alert")
+    
     @State private var appModel = AppModel()
     
     var body: some Scene {
@@ -22,6 +24,14 @@ struct DcmVisionApp: App {
                     minHeight: 500
                 )
                 .environment(appModel)
+                .alert("⚠️ Welcome to DcmVision ⚠️", isPresented: $alertForDemo) {
+                    Button("OK") {
+                        UserDefaults.standard.set(true, forKey: "alert")
+                        alertForDemo = false
+                    }
+                } message: {
+                    Text("This is a technical demo. Although functional it is best to use it in a controlled environment. This tool is not intended for clinical use and should not be used for medical diagnosis or treatment.")
+                }
         }
         .windowResizability(.contentSize)
         
