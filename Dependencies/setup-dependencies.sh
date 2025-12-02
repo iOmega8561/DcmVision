4614f8dc5bc38b7bf8411e1e1b5a9c82d790a571
iOmega8561 <giusepperocco38@gmail.com>
2025-12-02 23:25:22 +0100

    Added GPL licensing notice to source code files

--- Dependencies/setup-dependencies.sh
+++ Dependencies/setup-dependencies.sh
@@ -1,3 +1,20 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2025  Giuseppe Rocco
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
 # POSSIBLE VALUES:
 # 1) "xros"        for Apple Vision Pro
 # 2) "xrsimulator" for visionOS Simulator
